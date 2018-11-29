5c1e4e834576ee65a4c65fc5abdc8896f3aa1d42
schokkoteddy <abharath60@gmail.com>
2018-11-29 13:17:52 +0100

    exchanged the wrong files with the right ones

--- embedded/src/PosCalculator.h
+++
@@ -1,7 +0,0 @@
-#ifndef POSCALCULATOR_H_
-#define POSCALCULATOR_H_
-
-
-void posCalculator(int index, int pin);
-
-#endif
--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,8 +1,30 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <stdio.h>
-#include "Parameters.h"
-#include "PosCalculator.h"
+
+#define MAX_ACTUATORS 8
+#define MAX_STEPS 10
+
+#define actuator 4
+#define steps 4
+
+#define SERVOMIN  120 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  650 // this is the 'maximum' pulse length count (out of 4096)
+double targetPos[steps][actuator] = {{90, 180, 90, 0},{180, 90, 0, 90},{180, 0, 180, 0},{180, 0, 180, 90}};
+//double targetPos[actuator][steps] = {{0, 30, 60, 90,120, 90, 60, 30},{180, 90, 0, 90,90, 180, 90, 0,},{180, 0, 180, 0,90, 180, 90, 0,},{180, 0, 180, 90,90, 180, 90, 0,}};
+double currentPos[actuator] = {0, 0, 0, 0 };
+int currentStep = 0;
+int stepsDone = 0; 
+double cycleTime = 1000;
+double stepTime = cycleTime / steps;
+double startTime;
+//bool posReached[actuator] = {0, 0, 0, 0};
+bool quit = 0;
+//int servos[4] = {0,2,4,6}
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void posCalculator(int index, int pin);
 
 
 void setup()
@@ -12,25 +34,30 @@ void setup()
   Serial.println("Setup Done");
 
   pwm.begin();
-  pwm.setPWMFreq(100); // Set to whatever you like, we don't use it in this demo!
+  pwm.setPWMFreq(60); // Set to whatever you like, we don't use it in this demo!
 
   // if you want to really speed stuff up, you can go into 'fast 400khz I2C' mode
   // some i2c devices dont like this so much so if you're sharing the bus, watch
   // out for this!
   //Wire.setClock(400000);
-  for (int i = 0; i < steps; i++){
-      for (int j= 0; j < actuator; j++){
+  for (int i = 0; i < actuator; i++){
+      for (int j= 0; j < steps; j++){
           targetPos[i][j] = map(targetPos[i][j],0,180,SERVOMIN,SERVOMAX);
       }
   }
-  pwm.setPWM(1,0,SERVOMIN);
-  //delay(100);
+  pwm.setPWM(0,0,SERVOMIN);
+  pwm.setPWM(2,0,SERVOMIN);
+  pwm.setPWM(4,0,SERVOMIN);
+  pwm.setPWM(6,0,SERVOMIN);
   startTime = millis();
   }
 
 void loop()
 {
-    posCalculator(2, 2);
+    posCalculator(0,0);
+    posCalculator(0,2);
+    posCalculator(0,4);
+    posCalculator(0,6);
     if ((millis() - startTime) / ((stepsDone + 1) * stepTime) >= 1)
     {
         currentStep++;
@@ -41,17 +68,26 @@ void loop()
         }
     }
 
-    //Serial.println(((targetPos[2][0] - targetPos[2][currentStep]) / stepTime) /* * (millis() - startTime - stepsDone * stepTime) + targetPos[2][currentStep]*/);
-   // Serial.println(currentPos[2]);
+   Serial.println(currentStep);
+}
 
-    //Serial.println(millis());
-    /*
-      for (int i = 0; i < steps; i++){
-      for (int j= 0; j < actuator; j++){
-          Serial.print(targetPos[i][j]);
-          Serial.print(" ");
-      }
-      Serial.print("\n"); 
-  }
-  */
+void posCalculator(int index, int pin)
+{
+        if (currentStep == (steps - 1))
+    {
+        if(!(targetPos[index][0] - targetPos[index][currentStep]) ==0)
+        {
+        currentPos[index] = ((targetPos[index][0] - targetPos[index][currentStep]) / stepTime) * (millis() - startTime - stepsDone * stepTime) + targetPos[index][currentStep];
+        }
+                pwm.setPWM(pin, 0, currentPos[index]);
+        
+    }
+    else
+    {
+        if(!(targetPos[index][currentStep + 1] - targetPos[index][currentStep]) ==0){
+        currentPos[index] = ((targetPos[index][currentStep + 1] - targetPos[index][currentStep]) / stepTime) * (millis() - startTime - stepsDone * stepTime) + targetPos[index][currentStep];
+        }
+                pwm.setPWM(pin, 0, currentPos[index]);
+          
+    }
 }
